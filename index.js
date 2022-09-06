3e46b43ff98c430bfed7a9ab1079c65b1d2c0189
Madeleine Heineman <mheineman16@gmail.com>
2022-09-06 02:38:59 -0400

    commit tree bae861651ea90179e5586987bf47a1efbd24520c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(p1) {
+    return Math.abs(p1 - 42)
+}
+function distanceFromHqInFeet(p1) {
+    return Math.abs(p1 - 42) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264)
+}
+
+
+console.log(distanceTravelledInFeet(50, 58))
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination)
+    let dist = feet - 400
+    if (dist < 1) {
+      return 0
+    } 
+      else if (feet > 2500) {
+      return `cannot travel that far`
+    }
+      else if (feet > 2000) {
+      return 25
+    }
+        else if (dist < 2000) {
+      return Math.abs(dist * 0.02) 
+    }
+    
+  }
+
+// console.log(distanceTravelledInFeet(55, 45))
+// console.log(calculatesFarePrice(55, 45))
+  
+
+
+// function calculatesFarePrice(start, destination) {
+//     const feet = distanceTravelledInFeet(start, destination)
+//     const distance = feet - 400
+//     if distance === 0
+//         return "0"
+// }
